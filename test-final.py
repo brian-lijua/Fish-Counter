b9d86b5ae6d9a772fcc94256f1390608c7dbcf40
Brian <brian.lijua@pkmgroup.com>
2020-03-19 23:36:54 -0700

    Added parameter command and imporove logic efficiency

--- test-final.py
+++ test-final.py
@@ -4,6 +4,7 @@ from threading import Thread
 from imutils.video import FileVideoStream, FPS
 import cv2
 import time
+from pathlib import Path
 import numpy as np
 import multi_tracker as MultiTracker
 
@@ -18,12 +19,18 @@ FRAME_SCALE = 50
 
 fps = FPS()
 
-video_output_name: str = uuid.uuid4
+VIDEO_OUTPUT_FOLDER = 'video_output'
+VIDEO_OUPUT_NAME = '{}.avi'.format(uuid.uuid4().hex)
+VIDEO_FINAL_PATH = Path(os.path.join(VIDEO_OUTPUT_FOLDER, VIDEO_OUPUT_NAME))
 
 def write_to_video(dq, videoWidth, videoHeight):
-    videoWriter = cv2.VideoWriter('{}.avi'.format(video_output_name), cv2.VideoWriter_fourcc('M','J','P','G'), 30, (videoWidth, videoHeight))
+    if os.path.isdir(VIDEO_OUTPUT_FOLDER) != True:
+        os.mkdir(VIDEO_OUTPUT_FOLDER)
+
+    logger.info('Processing: {}'.format(VIDEO_OUPUT_NAME))
+    videoWriter = cv2.VideoWriter(os.path.join(VIDEO_OUTPUT_FOLDER, VIDEO_OUPUT_NAME), cv2.VideoWriter_fourcc(*'DIVX'), 30, (videoWidth, videoHeight))
     while True:
-        (total, frame) = dq.get()        
+        (total, frame) = dq.get()
         h, w = frame.shape[:2]
         c = int(w / 2)
         
@@ -31,7 +38,7 @@ def write_to_video(dq, videoWidth, videoHeight):
         frame = cv2.rectangle(frame, (38, 15), (300, 55), (0,0,255), -1)
         frame = cv2.putText(frame, 'Total: {}'.format(total), (50, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 255, 255), 2)
         
-        if frame is not None:            
+        if frame is not None:
             videoWriter.write(frame)
         cv2.waitKey(30)
         dq.task_done()
@@ -39,9 +46,7 @@ def write_to_video(dq, videoWidth, videoHeight):
 def run(video_path):
 
     if not os.path.isfile(video_path):
-        logger.error('{} path is invalid'.format(video_path))
-
-    logger.info('start processing: {}'.format(video_path))
+        logger.error('{} path is invalid'.format(video_path))    
 
     start_process_time = time.time()
 
@@ -59,7 +64,7 @@ def run(video_path):
 
     mt = MultiTracker.MultiTracker()
     mt.setThreshold(abs(0.8 * (vw / 2)))
-
+    
     subtractor = cv2.bgsegm.createBackgroundSubtractorGSOC(beta=0.03)
     
     fps.start()
@@ -75,7 +80,7 @@ def run(video_path):
         
         mask = grayFrame.copy()
         mask = subtractor.apply(mask)
-
+        
         trackers = mt.update(small_frame)
         if len(trackers) > 0:
             for tracker in trackers:
@@ -117,8 +122,7 @@ def run(video_path):
         small_frame = cv2.resize(small_frame, dsize=None, dst=None, fx=(2), fy=(2), interpolation=cv2.INTER_LANCZOS4)
 
         fps.stop()
-        fps.update()
-        # print(fps.fps())
+        fps.update()        
         
         dq.put((counter, small_frame))
         frame = cap.read()    
@@ -127,7 +131,13 @@ def run(video_path):
     cap.stop()
 
     end_process_time = time.time()
-    logger.info('Completed - duration: {}, video: {}'.format(int(end_process_time - start_process_time), video_path))
+    logger.info('Completed - duration: {}, video: {}'.format(int(end_process_time - start_process_time), VIDEO_FINAL_PATH.absolute()))
+    return VIDEO_FINAL_PATH.absolute()
 
 if __name__ == '__main__':
-    run('fish_counting_ai.mp4')
+    import argparse
+    parser = argparse.ArgumentParser(description='File path')
+    parser.add_argument('filepath', help='Enter the video absolute path')
+    args = parser.parse_args()
+
+    run(args.filepath)
